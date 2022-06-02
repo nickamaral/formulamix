380a928acde48d34b6bb51fafa79c52210f14712
Nicolas Amaral <nicolasamaralsilva09@gmail.com>
2022-06-02 19:57:39 -0300

    commit inicial

---
+++ src/main/java/br/com/formulamix/produtosmix/ProdutosmixApplication.java
@@ -0,0 +1,13 @@
+package br.com.formulamix.produtosmix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosmixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosmixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/formulamix/produtosmix/ProdutosmixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.formulamix.produtosmix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosmixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
